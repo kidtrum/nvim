ae53a85e00b9907b615d317aec4d9aad972dbb7d
Nguyen Nguyen <nguyen.nguyen.ym@renesas.com>
2024-06-30 11:34:09 +0700

    Update for tabline

--- lua/config/plugins/tabline.lua
+++ lua/config/plugins/tabline.lua
@@ -5,8 +5,16 @@ return {
     config = function()
         local tabline = require("tabline")
 
-        tabline.setup({})
-
-        vim.opt.showtabline = 1
+        tabline.setup({
+            show_index = true,           -- show tab index
+            show_modify = true,          -- show buffer modification indicator
+            show_icon = true,           -- show file extension icon
+            fnamemodify = ':t',          -- file name modifier string
+            -- can be a function to modify buffer name
+            modify_indicator = '[+]',    -- modify indicator
+            no_name = 'No name',         -- no name buffer name
+            brackets = { "", "" },     -- file name brackets surrounding
+            inactive_tab_max_length = 20  -- max length of inactive tab titles, 0 to ignore
+        })
     end,
 }
